de329d27706fbaae3baf59aa402864c5bb80b935
jiashusu <8326305209sjs@gmail.com>
2026-05-11 18:06:26 -0500

    Add app icon

--- scripts/package_app.sh
+++ scripts/package_app.sh
@@ -5,13 +5,15 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 APP_NAME="Mac模式.app"
 APP_DIR="$ROOT/build/$APP_NAME"
 BIN_DIR="$APP_DIR/Contents/MacOS"
+RESOURCES_DIR="$APP_DIR/Contents/Resources"
 
 cd "$ROOT"
 swift build -c release
 
 rm -rf "$APP_DIR"
-mkdir -p "$BIN_DIR"
+mkdir -p "$BIN_DIR" "$RESOURCES_DIR"
 cp "$ROOT/.build/release/MacModeMenu" "$BIN_DIR/MacModeMenu"
 cp "$ROOT/Bundle/Info.plist" "$APP_DIR/Contents/Info.plist"
+cp "$ROOT/Bundle/Resources/AppIcon.icns" "$RESOURCES_DIR/AppIcon.icns"
 
 echo "$APP_DIR"
